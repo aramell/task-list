4b19acba13ba5456e67eb12cd3a31198f1cdf9a7
Andrew <github email address>
2017-07-28 18:15:34 -0400

    added login

--- app/controllers/usercontroller.rb
+++ app/controllers/usercontroller.rb
@@ -1,16 +1,34 @@
 class UserController < ApplicationController
 
-    get '/newuser' do
+    get '/signup' do
+        if logged_in?
+            redirect to "/lists/showlist"
+        else
 
         erb :'user/newuser'
+        end
     end
-    post '/users' do
-        @user = User.create(params[:user])
-        binding.pry
+  
+    post '/signup' do
+        
+        @user = User.create(username: params[:user][:username], password_digest: params[:user][:username])
+            session[:user_id] = @user.id
         redirect to '/lists/showlist'
     end
     get '/login' do
-
-        erb :'user/login'
+        if !logged_in?
+            erb :"/user/login"
+          else
+            redirect to "/lists/showlist"
         end
+    end
+    post '/login' do    
+        @user = User.find_by(:username =>[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+             redirect to "/lists/showlist"
+             else
+            redirect to '/login'
+        end
+    end
  end
